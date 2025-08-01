0c06a11876d642177e3e8b68a561aff82d01ecff
goguma <doffltm112543@gmail.com>
2025-08-01 17:49:59 +0900

    메인 컨트롤러 내용 추가

---
+++ src/main/java/kr/goguma/mcdonald/maincontroller.java
@@ -0,0 +1,13 @@
+package kr.goguma.mcdonald;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String getMainPage() {
+        return "main";
+    }
+}
